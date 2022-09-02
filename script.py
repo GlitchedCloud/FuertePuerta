c994809673680cb8def1f385a6c8a9579e830759
GlitchedCloud <armondcastor@gmail.com>
2022-09-02 14:28:32 +1000

    Ta-da...

--- script.py
+++ script.py
@@ -1,19 +1,10 @@
-from time import sleep
-from signal import pause
-from subprocess import run, Popen, PIPE
+from subprocess import run
 from gpiozero import MotionSensor
 
-sleep(600) # This gives you 10 minutes to leave your home
-pir = MotionSensor(24)
+pir = MotionSensor(24) #The GPIO your sensor is on.
 
 while True:
-p1 = Popen([“ping”, “-c”, “1”, “192.168.0.5”], stdout=PIPE)
-stdout_value = p1.communicate()[0]
-if b’Destination Host Unreachable’ not in stdout_value:
-break
-sleep(2)
+    pir.wait_for_motion()
+    run(“mpg321 'https://raw.githubusercontent.com/GlitchedCloud/FuertePuerta/main/sound.mp3'”, shell=True)
 
-sleep(5)
-
-pir.wait_for_motion()
-run(“mpg123 ~/entrancemusic/’Sexy Boy (Shawn Michaels).mp3′”, shell=True)
+    pir.wait_for_no_motion()
